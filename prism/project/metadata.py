71d174db8819dc573fee00a61625592d213ec811
Nathan Henderson <nhenderson@radiancetech.com>
2022-04-25 22:54:33 +0000

    Added unit tests for dataclass.

---
+++ prism/project/metadata.py
@@ -0,0 +1,104 @@
+"""
+Contains all metadata related to paticular GitHub repositories.
+"""
+
+from dataclasses import dataclass
+
+import seutil as su
+
+METADATA_FIELDS = [
+    'full_name',
+    'short_name',
+    'id',
+    'url',
+    'sha',
+    'tag',
+    'dependencies',
+    'build_cmd',
+    'install_cmd',
+    'serapi_options',
+    'ignore_path_regex',
+    'group'
+]
+
+
+def load_metadata_from_yaml(filepath) -> list:
+    """
+    Create list of ProjectMetadata() objects.
+    """
+    yaml_lst = su.io.load(filepath)
+
+    # Store as list (if only a single project exists in YAML file,
+    # seutil.io stores it as a single dict)
+    if type(yaml_lst) == dict:
+        yaml_lst = [yaml_lst]
+
+    project_metadata = []
+
+    for metadata_dict in yaml_lst:
+        missing_fields = [
+            field for field in METADATA_FIELDS
+            if field not in metadata_dict.keys()
+        ]
+        if len(missing_fields) > 0:
+            print(
+                "Missing the follwing metadata from %s: %s" %
+                (metadata_dict['full_name'],
+                 ", ".join(missing_fields)))
+            continue
+
+        project_metadata.append(
+            ProjectMetadata(
+                metadata_dict['full_name'],
+                metadata_dict['short_name'],
+                metadata_dict['id'],
+                metadata_dict['url'],
+                metadata_dict['sha'],
+                metadata_dict['tag'],
+                metadata_dict['dependencies'],
+                metadata_dict['build_cmd'],
+                metadata_dict['install_cmd'],
+                metadata_dict['ignore_path_regex'],
+                metadata_dict['serapi_options'],
+                metadata_dict['group']))
+    return project_metadata
+
+
+@dataclass
+class ProjectMetadata:
+    """
+    Class containing the metadata for a single GitHub repo.
+    """
+
+    full_name: str
+    short_name: str
+    id: str
+    url: str
+    sha: str
+    tag: str
+    dependencies: list
+    build_cmd: str
+    install_cmd: str
+    ignore_path_regex: str
+    serapi_options: str
+    group: str
+
+    def serialize_to_str(self) -> str:
+        """
+        Serialize metadata to string format as .yml data.
+
+        Returns
+        -------
+            str: string representation of metadata dataclass
+        """
+        return su.io.serialize(self.__dict__, fmt='yaml')
+
+    def serialize_to_file(self, output_filepath):
+        """
+        Serialize metadata and writes to .yml file.
+
+        Args:
+        ----
+            output_filepath (str): filepath to .yml file
+        """
+        su.io.dump(output_filepath, self.__dict__, fmt=su.io.Fmt.yaml)
---
+++ prism/project/tests/test_metadata.py
@@ -0,0 +1,88 @@
+"""
+"" Test suite for `prism.project.metadata`.
+""" ""
+
+import os
+import unittest
+
+from prism.project.metadata import load_metadata_from_yaml
+
+
+class TestProjectMetadata(unittest.TestCase):
+    """
+    Test suite for `ProjectMetadata`.
+    """
+
+    def test_load_metadata_from_yaml(self):
+        """
+        Ensure import .yaml file can be deserialized properly.
+        """
+        yaml_path = "prism/project/tests/projects.yml"
+        metadata_objs = load_metadata_from_yaml(yaml_path)
+
+        # Only two projects in the YAML file contain all 12 attributes
+        self.assertEqual(len(metadata_objs), 2)
+
+    def test_serialize_to_str(self):
+        """
+        Ensure deserialized dataclass object can be properly serialized.
+        """
+        yaml_path = "prism/project/tests/projects.yml"
+        metadata_objs = load_metadata_from_yaml(yaml_path)
+        metadata_obj = metadata_objs[0]
+
+        # Only two projects in the YAML file contain all 12 attributes
+        self.assertEqual(
+            str(metadata_obj.serialize_to_str()),
+            "{\'full_name\': "
+            + "\'DistributedComponents_disel\', \'short_name\': \'disel\', "
+            + "\'id\': \'disel\', \'url\': "
+            + "\'https://github.com/DistributedComponents/disel\', "
+            + "\'sha\': \'e8aa80c486ec618888b6d0c801da7f61b6046daa\', "
+            + "\'tag\': \'v2.1\', \'dependencies\': "
+            + "[\'math-comp_math-comp\', \'imdea-software_fcsl-pcm\'], "
+            + "\'build_cmd\': \'make -j8 -C Core\', "
+            + "\'install_cmd\': \'make -C Core install\', "
+            + "\'ignore_path_regex\': \'Core/InjectionOld\\\\.v|Examples/.*\', "
+            + "\'serapi_options\': \'-R Core,DiSeL\', \'group\': \'t3\'}")
+
+    def test_serialize_to_file(self):
+        """
+        Tests YAML files are serialized properly.
+        """
+        yaml_path = "prism/project/tests/projects.yml"
+        metadata_objs = load_metadata_from_yaml(yaml_path)
+        metadata_obj = metadata_objs[0]
+        metadata_obj.serialize_to_file("test.yml")
+
+        # Test that .yml file is created properly
+        self.assertTrue(os.path.isfile("test.yml"))
+
+        new_metadata_obj = load_metadata_from_yaml("test.yml")[0]
+
+        # Test that data is consistent after being
+        # serialized/deserialized
+        self.assertEqual(metadata_obj.full_name, new_metadata_obj.full_name)
+        self.assertEqual(metadata_obj.short_name, new_metadata_obj.short_name)
+        self.assertEqual(metadata_obj.id, new_metadata_obj.id)
+        self.assertEqual(metadata_obj.url, new_metadata_obj.url)
+        self.assertEqual(metadata_obj.sha, new_metadata_obj.sha)
+        self.assertEqual(metadata_obj.tag, new_metadata_obj.tag)
+        self.assertEqual(
+            metadata_obj.dependencies,
+            new_metadata_obj.dependencies)
+        self.assertEqual(metadata_obj.build_cmd, new_metadata_obj.build_cmd)
+        self.assertEqual(metadata_obj.install_cmd, new_metadata_obj.install_cmd)
+        self.assertEqual(
+            metadata_obj.ignore_path_regex,
+            new_metadata_obj.ignore_path_regex)
+        self.assertEqual(
+            metadata_obj.serapi_options,
+            new_metadata_obj.serapi_options)
+        self.assertEqual(metadata_obj.group, new_metadata_obj.group)
+
+        os.remove("test.yml")
+
+
+if __name__ == '__main__':
+    unittest.main()
