6e53c7b40fb48c574e8e88d32035e3357d12d13f
Nathan Henderson <nhenderson@radiancetech.com>
2022-04-26 18:37:36 +0000

    Refactored  to use seutil.io.deserialize.

--- prism/project/metadata.py
+++ prism/project/metadata.py
@@ -4,67 +4,23 @@ Contains all metadata related to paticular GitHub repositories.
 
 import os
 from dataclasses import dataclass
+from typing import List
 
 import seutil as su
-
-METADATA_FIELDS = [
-    'full_name',
-    'short_name',
-    'id',
-    'url',
-    'sha',
-    'tag',
-    'dependencies',
-    'build_cmd',
-    'install_cmd',
-    'serapi_options',
-    'ignore_path_regex',
-    'group'
-]
 
 
 def load_metadata_from_yaml(filepath) -> list:
     """
     Create list of `ProjectMetadata` objects.
     """
-    yaml_lst = su.io.load(filepath)
+    data = su.io.load(filepath, su.io.Fmt.yaml)
+    project_metadata: List[ProjectMetadata] = [
+        su.io.deserialize(project,
+                          ProjectMetadata)
+        for project in data
+        if set(project.keys()) == set(ProjectMetadata.__dataclass_fields__)
+    ]
 
-    # Store as list (if only a single project exists in YAML file,
-    # seutil.io stores it as a single dict)
-    if type(yaml_lst) == dict:
-        yaml_lst = [yaml_lst]
-
-    project_metadata = []
-
-    for metadata_dict in yaml_lst:
-        missing_fields = [
-            field for field in METADATA_FIELDS
-            if field not in metadata_dict.keys()
-        ]
-
-        # Print if a project in YAML file is missing any
-        # required metadata and skip
-        if len(missing_fields) > 0:
-            print(
-                "Missing the following metadata from %s: %s" %
-                (metadata_dict['full_name'],
-                 ", ".join(missing_fields)))
-            continue
-
-        project_metadata.append(
-            ProjectMetadata(
-                metadata_dict['full_name'],
-                metadata_dict['short_name'],
-                metadata_dict['id'],
-                metadata_dict['url'],
-                metadata_dict['sha'],
-                metadata_dict['tag'],
-                metadata_dict['dependencies'],
-                metadata_dict['build_cmd'],
-                metadata_dict['install_cmd'],
-                metadata_dict['ignore_path_regex'],
-                metadata_dict['serapi_options'],
-                metadata_dict['group']))
     return project_metadata
 
 
