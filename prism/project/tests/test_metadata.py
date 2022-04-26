1709ca0a1a5949312fbd48fdb4c9dc7ed1a6bb77
Nathan Henderson <nhenderson@radiancetech.com>
2022-04-26 15:48:08 +0000

    Added functionality to write multiple projects to a single YAML file.

--- prism/project/tests/test_metadata.py
+++ prism/project/tests/test_metadata.py
@@ -83,6 +83,78 @@ class TestProjectMetadata(unittest.TestCase):
 
         os.remove("test.yml")
 
+    def test_serialize_to_file_multi(self):
+        """
+        Tests YAML file with multiple projects are serialized properly.
+        """
+        yaml_path = "prism/project/tests/projects.yml"
+        metadata_objs = load_metadata_from_yaml(yaml_path)
+        metadata_obj_1 = metadata_objs[0]
+        metadata_obj_2 = metadata_objs[1]
+        metadata_obj_1.serialize_to_file("prism/project/tests/test.yml")
+        metadata_obj_2.serialize_to_file("prism/project/tests/test.yml")
+
+        # Test that .yml file is created properly
+        self.assertTrue(os.path.isfile("prism/project/tests/test.yml"))
+
+        # Test that both metadata objects are deserialized correctly
+        new_metadata_lst = load_metadata_from_yaml(
+            "prism/project/tests/test.yml")
+        self.assertEqual(len(new_metadata_lst), 2)
+
+        # Test that data from the first project is consistent
+        # after being serialized/deserialized
+        new_metadata_obj_1 = new_metadata_lst[0]
+        new_metadata_obj_2 = new_metadata_lst[1]
+
+        self.assertEqual(metadata_obj_1.full_name, new_metadata_obj_1.full_name)
+        self.assertEqual(
+            metadata_obj_1.short_name,
+            new_metadata_obj_1.short_name)
+        self.assertEqual(metadata_obj_1.id, new_metadata_obj_1.id)
+        self.assertEqual(metadata_obj_1.url, new_metadata_obj_1.url)
+        self.assertEqual(metadata_obj_1.sha, new_metadata_obj_1.sha)
+        self.assertEqual(metadata_obj_1.tag, new_metadata_obj_1.tag)
+        self.assertEqual(
+            metadata_obj_1.dependencies,
+            new_metadata_obj_1.dependencies)
+        self.assertEqual(metadata_obj_1.build_cmd, new_metadata_obj_1.build_cmd)
+        self.assertEqual(
+            metadata_obj_1.install_cmd,
+            new_metadata_obj_1.install_cmd)
+        self.assertEqual(
+            metadata_obj_1.ignore_path_regex,
+            new_metadata_obj_1.ignore_path_regex)
+        self.assertEqual(
+            metadata_obj_1.serapi_options,
+            new_metadata_obj_1.serapi_options)
+        self.assertEqual(metadata_obj_1.group, new_metadata_obj_1.group)
+
+        self.assertEqual(metadata_obj_2.full_name, new_metadata_obj_2.full_name)
+        self.assertEqual(
+            metadata_obj_2.short_name,
+            new_metadata_obj_2.short_name)
+        self.assertEqual(metadata_obj_2.id, new_metadata_obj_2.id)
+        self.assertEqual(metadata_obj_2.url, new_metadata_obj_2.url)
+        self.assertEqual(metadata_obj_2.sha, new_metadata_obj_2.sha)
+        self.assertEqual(metadata_obj_2.tag, new_metadata_obj_2.tag)
+        self.assertEqual(
+            metadata_obj_2.dependencies,
+            new_metadata_obj_2.dependencies)
+        self.assertEqual(metadata_obj_2.build_cmd, new_metadata_obj_2.build_cmd)
+        self.assertEqual(
+            metadata_obj_2.install_cmd,
+            new_metadata_obj_2.install_cmd)
+        self.assertEqual(
+            metadata_obj_2.ignore_path_regex,
+            new_metadata_obj_2.ignore_path_regex)
+        self.assertEqual(
+            metadata_obj_2.serapi_options,
+            new_metadata_obj_2.serapi_options)
+        self.assertEqual(metadata_obj_2.group, new_metadata_obj_2.group)
+
+        os.remove("prism/project/tests/test.yml")
+
 
 if __name__ == '__main__':
     unittest.main()
