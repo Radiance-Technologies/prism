aee0475037999436eff1233aefddae12beec6d8d
Nathan Henderson <nhenderson@radiancetech.com>
2022-04-27 16:54:11 +0000

    Updated metadata dataclass to contain attributes from https://rsngit.radiancetech.com/pearls/coq-pearls/-/blob/feature-project-metadata/metadata/ProjectMetadataSpecifications.md. Updated unit tests to reflect new fields and when option/required fields are missing.

--- prism/project/metadata.py
+++ prism/project/metadata.py
@@ -3,8 +3,8 @@ Contains all metadata related to paticular GitHub repositories.
 """
 
 import os
-from dataclasses import dataclass
-from typing import List
+from dataclasses import dataclass, fields
+from typing import List, Optional
 
 import seutil as su
 
@@ -16,9 +16,7 @@ def load_metadata_from_yaml(filepath) -> list:
     data = su.io.load(filepath, su.io.Fmt.yaml)
     project_metadata: List[ProjectMetadata] = [
         su.io.deserialize(project,
-                          ProjectMetadata)
-        for project in data
-        if set(project.keys()) == set(ProjectMetadata.__dataclass_fields__)
+                          ProjectMetadata) for project in data
     ]
 
     return project_metadata
@@ -27,21 +25,34 @@ def load_metadata_from_yaml(filepath) -> list:
 @dataclass(order=True)
 class ProjectMetadata:
     """
-    Class containing the metadata for a single project.
+    Class contains the metadata for a single project.
     """
 
-    full_name: str
-    short_name: str
-    id: str
-    url: str
-    sha: str
-    tag: str
-    dependencies: list
-    build_cmd: str
-    install_cmd: str
-    ignore_path_regex: str
-    serapi_options: str
-    group: str
+    project_name: str = None
+    project_url: str = None
+    serapi_options: str = None
+    coq_version: List[str] = None
+    serapi_version: List[str] = None
+    build_cmd: str = None
+    opam_repos: Optional[List[str]] = None
+    commit_sha: Optional[str] = None
+    ignore_path_regex: Optional[List[str]] = None
+    install_cmd: Optional[str] = None
+    clean_cmd: Optional[str] = None
+
+    def __post_init__(self) -> None:
+        """
+        Raise exception if required fields missing.
+        """
+        missing_required_fields = [
+            field.name for field in fields(self) if
+            field.type != Optional[field.type] and getattr(self,
+                                                           field.name) is None
+        ]
+        if len(missing_required_fields) > 0:
+            raise TypeError(
+                "Missing following required field(s): %s"
+                % ", ".join(missing_required_fields))
 
     def serialize_to_file(self, output_filepath: os.PathLike) -> None:
         """
