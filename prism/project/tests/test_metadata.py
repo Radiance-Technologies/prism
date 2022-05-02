66bc8b807b3da31903ec61f93799be4109e4eb0d
Nathan Henderson <nhenderson@radiancetech.com>
2022-05-02 16:41:43 +0000

    Changed serialization functionality to class methods.

--- prism/project/tests/test_metadata.py
+++ prism/project/tests/test_metadata.py
@@ -38,7 +38,7 @@ class TestProjectMetadata(unittest.TestCase):
         yaml_path = "prism/project/tests/projects.yml"
         metadata_objs = load_metadata_from_yaml(yaml_path)
         metadata_obj = metadata_objs[0]
-        metadata_obj.serialize_to_file("prism/project/tests/test.yml")
+        metadata_obj.dump([metadata_obj], "prism/project/tests/test.yml")
 
         # Test that .yml file is created properly
         self.assertTrue(os.path.isfile("prism/project/tests/test.yml"))
@@ -52,16 +52,16 @@ class TestProjectMetadata(unittest.TestCase):
 
         os.remove("prism/project/tests/test.yml")
 
-    def test_serialize_to_file_multi(self):
+    def test_serialize_to_existing_file(self):
         """
-        Tests YAML file with multiple projects are serialized properly.
+        Tests that appending to an existing YAML file is working.
         """
         yaml_path = "prism/project/tests/projects.yml"
         metadata_objs = load_metadata_from_yaml(yaml_path)
         metadata_obj_1 = metadata_objs[0]
         metadata_obj_2 = metadata_objs[1]
-        metadata_obj_1.serialize_to_file("prism/project/tests/test.yml")
-        metadata_obj_2.serialize_to_file("prism/project/tests/test.yml")
+        metadata_obj_1.dump([metadata_obj_1], "prism/project/tests/test.yml")
+        metadata_obj_2.dump([metadata_obj_2], "prism/project/tests/test.yml")
 
         # Test that .yml file is created properly
         self.assertTrue(os.path.isfile("prism/project/tests/test.yml"))
@@ -78,6 +78,32 @@ class TestProjectMetadata(unittest.TestCase):
 
         self.assertEqual(new_metadata_obj_1, metadata_obj_1)
         self.assertEqual(new_metadata_obj_2, metadata_obj_2)
+
+        os.remove("prism/project/tests/test.yml")
+
+    def test_serialize_to_file_multi(self):
+        """
+        Tests that serialization of a list of metadata objecs works.
+        """
+        yaml_path = "prism/project/tests/projects.yml"
+        metadata_objs = load_metadata_from_yaml(yaml_path)
+        metadata_objs[0].dump(metadata_objs, "prism/project/tests/test.yml")
+
+        # Test that .yml file is created properly
+        self.assertTrue(os.path.isfile("prism/project/tests/test.yml"))
+
+        # Test that both metadata objects are deserialized correctly
+        new_metadata_lst = load_metadata_from_yaml(
+            "prism/project/tests/test.yml")
+        self.assertEqual(len(new_metadata_lst), 2)
+
+        # Test that data from the first project is consistent
+        # after being serialized/deserialized
+        new_metadata_obj_1 = new_metadata_lst[0]
+        new_metadata_obj_2 = new_metadata_lst[1]
+
+        self.assertEqual(new_metadata_obj_1, metadata_objs[0])
+        self.assertEqual(new_metadata_obj_2, metadata_objs[1])
 
         os.remove("prism/project/tests/test.yml")
 
