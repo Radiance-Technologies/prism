ef3964a225d616b10a7e3bb9f00efe2f98ca1f4e
Andrew Gardner <Andrew.Gardner@radiancetech.com>
2022-05-09 16:08:51 +0000

    Remove single-use redundant scripts, clean up docs, and update metadata spec.

--- repos/scripts/process_git_coq_search.py
+++ repos/scripts/process_git_coq_search.py
@@ -1,24 +1,24 @@
+"""
+Extract github links to significantly Coq based projects.
+
+This script processes repositories found by Tom Reichel here:
+https://github.com/a-gardner1/coq-pearls/issues/2#issuecomment-1100753597
+
+This text file was hand-annotated using a '*' to indicate projects
+which were almost entirely Coq based. The decision to exclude
+projects which had substantial non-Coq code sections was based on
+the assumption that such projects would be more complex to build/rebuild
+or that they would require additional prerequisites which had the
+potential to balloon dev environment size or time to configure.
+
+This script turns that information into a json file for ease of use
+and manipulation in python.
+"""
+
 import json
 import re
 
-
 if __name__ == "__main__":
-    """
-    Extract github links to significantly Coq based projects.
-
-    This script processes repositories found by Tom Reichel here:
-    https://github.com/a-gardner1/coq-pearls/issues/2#issuecomment-1100753597
-
-    This text file was hand-annotated using a '*' to indicate projects
-    which were almost entirely Coq based. The decision to exclude
-    projects which had substantial non-Coq code sections was based on
-    the assumption that such projects would be more complex to build/rebuild
-    or that they would require additional prerequisites which had the potential
-    to balloon dev environment size or time to configure.
-
-    This script turns that information into a json file for ease of use 
-    and manipulation in python.
-    """
 
     file_in = open("./commit-then-star-sort.txt", 'r')
     lines = []
@@ -34,8 +34,7 @@ if __name__ == "__main__":
         match = regex.match(line)
         if match:
             repos.append(match.groups()[0])
-    
+
     print(repos, len(repos))
     with open("commit-then-star-sort.json", 'w') as outfile:
         json.dump(repos, outfile)
-
--- repos/scripts/process_repos_list.py
+++ repos/scripts/process_repos_list.py
@@ -1,22 +1,21 @@
+"""
+Extract repository name, link, and number of commits.
+
+This script processes the hand-annotated repos file
+made by Maxwell Reeser to assist in establishing correct
+correspondence between CoqGym's projects and their
+publicly available github repositories. Each repository's
+name was searched on google and the repository which seemed
+to best fit that included in CoqGym was listed as the proper
+link. The number of commits for each repo was also included.
+
+This script turns that information into a json file of triples
+for ease of use and manipulation in python.
+"""
 import json
 import re
 
-
 if __name__ == "__main__":
-    """
-    Extract repository name, link, and number of commits.
-
-    This script processes the hand-annotated repos file 
-    made by Maxwell Reeser to assist in establishing correct 
-    correspondence between CoqGym's projects and their 
-    publicly available github repositories. Each repository's
-    name was searched on google and the repository which seemed
-    to best fit that included in CoqGym was listed as the proper
-    link. The number of commits for each repo was also included.
-
-    This script turns that information into a json file of triples
-    for ease of use and manipulation in python.
-    """
 
     file_in = open("./repos", 'r')
     lines = []
@@ -26,10 +25,10 @@ if __name__ == "__main__":
 
     line_tuples = []
 
-    for i in range(int(len(lines)/2)):
-        line_tuples.append((lines[i*2],lines[i*2+1]))
+    for i in range(int(len(lines) / 2)):
+        line_tuples.append((lines[i * 2], lines[i * 2 + 1]))
 
-    #line_triples = [(k.split("-"), y) for (k, y) in line_tuples]
+    # line_triples = [(k.split("-"), y) for (k, y) in line_tuples]
 
     regex = re.compile(r"(.*) - (https.*)")
 
@@ -38,12 +37,18 @@ if __name__ == "__main__":
     for repo_tuple in line_tuples:
         match = regex.match(repo_tuple[0])
         if match:
-            line_triples.append((match.groups()[0], match.groups()[1].strip("\n"), repo_tuple[1].strip("\n")))
+            line_triples.append(
+                (
+                    match.groups()[0],
+                    match.groups()[1].strip("\n"),
+                    repo_tuple[1].strip("\n")))
         else:
-            line_triples.append((repo_tuple[0].strip("\n"), "", repo_tuple[1].strip("\n")))
-    
+            line_triples.append(
+                (repo_tuple[0].strip("\n"),
+                 "",
+                 repo_tuple[1].strip("\n")))
+
     print(line_triples, len(line_triples))
     out = json.dumps(line_triples)
     with open("repos.json", 'w') as outfile:
         json.dump(line_triples, outfile)
-
