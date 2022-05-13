3d0fa1ca3816c362699052c41c24e6a4c2ddda56
Andrew Gardner <Andrew.Gardner@radiancetech.com>
2022-05-13 18:56:23 +0000

    Move scripts/count.py to scripts/data and add positional arguments.

--- scripts/data/count.py
+++ scripts/data/count.py
@@ -1,16 +1,16 @@
 """
-Module for downloading projects.
+Count sentences and proofs contained in projects.
 """
 import json
 import os
-import sys
 from multiprocessing import Pool
+from typing import Dict, Tuple
 
 from prism.project.base import SEM
 from prism.project.repo import ProjectRepo
 
 
-def proofs(path: os.PathLike):
+def proofs(path: os.PathLike) -> Tuple[str, int]:
     """
     Count number proofs.
     """
@@ -29,7 +29,7 @@ def proofs(path: os.PathLike):
     return os.path.basename(path), count
 
 
-def sentences(path: os.PathLike):
+def sentences(path: os.PathLike) -> Tuple[str, int]:
     """
     Count number of sentences.
     """
@@ -46,44 +46,55 @@ def sentences(path: os.PathLike):
     return os.path.basename(path), count
 
 
-def all_counts(path: os.PathLike):
+def all_counts(path: os.PathLike) -> Dict[str, Dict[str, int]]:
     """
     Generate counting for sentences and proofs.
     """
-    name, p = proofs(path)
-    name, s = sentences(path)
-    return {
-        name: {
-            "proofs": p,
-            "sentences": s
+    try:
+        name, p = proofs(path)
+        name, s = sentences(path)
+        return {
+            name: {
+                "proofs": p,
+                "sentences": s
+            }
         }
-    }
+    except Exception as e:
+        print(f"{type(e).__name__} encountered in {path}")
+        raise e
 
 
 if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "root_dir",
+        help="The root directory containing project subdirectories.")
+    parser.add_argument(
+        "out_file",
+        help="The path to a JSON file to which results will be dumped.")
+    parser.add_argument(
+        "num_workers",
+        help="The number of parallel processes to use.",
+        type=int,
+        nargs='?',
+        default=1)
+    args = parser.parse_args()
 
-    root_dir = sys.argv[1]
-    json_file = sys.argv[2]
-    n_procs = sys.argv[3]
-    mode = sys.argv[4]
-    dirs = [os.path.join(root_dir, path) for path in os.listdir(root_dir)]
-
-    if mode == 'proofs':
-        func = proofs
-    elif mode == 'sentences':
-        func = sentences
-    else:
-        raise ValueError(f"Invalid mode: {mode}")
+    dirs = [
+        os.path.join(args.root_dir,
+                     path) for path in os.listdir(args.root_dir)
+    ]
 
     counts = {}
-    with Pool(int(n_procs)) as p:
+    with Pool(args.num_workers) as p:
         data = p.map(all_counts, dirs)
     for d in data:
         counts.update(d)
 
-    total_proof = 0
-    total_sentence = 0
-    with open(json_file, 'w') as fp:
+    total_proof = sum([d["proofs"] for d in counts.values()])
+    total_sentence = sum([d["sentences"] for d in counts.values()])
+    with open(args.out_file, 'w') as fp:
         json.dump(counts, fp)
     print(f"Total proofs: {total_proof}")
     print(f"Total sentences: {total_sentence}")
