494406acb0cde902cfeb695d90b67d779b577c30
Maxwell Reeser <maxwell.reeser@radiancetech.com>
2022-07-12 14:48:26 +0000

    Fixing flake8

--- prism/data/cache.py
+++ prism/data/cache.py
@@ -2,7 +2,6 @@
 Module for looping over dataset and extracting caches.
 """
 import os
-import time
 
 from tqdm.contrib.concurrent import process_map
 
@@ -12,6 +11,9 @@ from .dataset import CoqGymBaseDataset
 
 
 def cache_extract(project):
+    """
+    Extract cache from series of commits.
+    """
     os.chdir("./{0}".format(project.name))
     iterator = CommitIterator(project, project.commit().hexsha)
     counter = 0
@@ -24,12 +26,27 @@ def cache_extract(project):
 
 
 class Looper:
+    """
+    Loop through all commits in all projects.
+    """
 
     def __init__(self, dataset: CoqGymBaseDataset):
+        """
+        Initialize Looper object.
+
+        Parameters
+        ----------
+        dataset : CoqGymBaseDataset
+            Dataset object used to loop through all
+            commits in all projects, building each
+            commit and extracting the build cache.
+        """
         self.dataset = dataset
-        return
 
     def __call__(self, working_dir):
+        """
+        Run looping functionality.
+        """
         os.chdir(working_dir)
         process_map(
             cache_extract,
--- prism/data/tests/test_cache.py
+++ prism/data/tests/test_cache.py
@@ -15,74 +15,89 @@ from prism.project.metadata.storage import MetadataStorage
 
 
 class DatasetFactory:
+    """
+    Create test dataset.
 
-    def __init__(cls):
-        cls.test_path = os.path.dirname(__file__)
+    Useful for debugging if interactability is
+    desired in an interpreter session. 
+    """
+
+    def __init__(self):
+        self.test_path = os.path.dirname(__file__)
         # HEAD commits as of March 14, 2022
-        cls.project_names = {"bellantonicook",
+        self.project_names = {"bellantonicook",
                              "circuits",
                              "coq-cunit"}
-        cls.target_projects = {
+        self.target_projects = {
             "bellantonicook": "davidnowak/bellantonicook",
             "circuits": "coq-contribs/circuits",
             "coq-cunit": "clarus/coq-cunit"
         }
-        cls.commit_shas = {
+        self.commit_shas = {
             "bellantonicook": "1f03b9296104646ddc2b2b4b12e35a6619c17a99",
             "circuits": "f2cec6067f2c58e280c5b460e113d738b387be15",
             "coq-cunit": "fa20b6450f5efbd1e293d0f4e4f6ce599eeb25f0"
         }
-        cls.build_cmds = {
+        self.build_cmds = {
             "bellantonicook": ["make"],
             "circuits": ["make"],
             "coq-cunit": ["./configure.sh",
                           "make"]
         }
 
-        cls.repo_paths = {}
-        cls.repos = {}
-        cls.projects = {}
-        cls.metadatas = {}
-        cls.metadata_storage = MetadataStorage()
+        self.repo_paths = {}
+        self.repos = {}
+        self.projects = {}
+        self.metadatas = {}
+        self.metadata_storage = MetadataStorage()
 
-        cls.init_repo_paths()
-        cls.insert_metadata()
-        cls.init_projects()
-        cls.dataset = CoqGymBaseDataset(
+        self.init_repo_paths()
+        self.insert_metadata()
+        self.init_projects()
+        self.dataset = CoqGymBaseDataset(
             project_class=ProjectRepo,
-            projects=cls.projects)
+            projects=self.projects)
 
-    def init_repo_paths(cls):
-        for project_name, project in cls.target_projects.items():
-            project_path = os.path.join(cls.test_path, project_name)
-            cls.repo_paths[project_name] = project_path
+    def init_repo_paths(self):
+        """
+        Initialize repo paths.
+        """
+        for project_name, project in self.target_projects.items():
+            project_path = os.path.join(self.test_path, project_name)
+            self.repo_paths[project_name] = project_path
             try:
                 repo = git.Repo.clone_from(
                     f"https://github.com/{project}",
                     project_path)
             except git.GitCommandError:
                 repo = git.Repo(project_path)
-            cls.repos[project_name] = repo
+            self.repos[project_name] = repo
             # TODO: Use real metadata and test building
-            cls.metadatas[project_name] = ProjectMetadata(
+            self.metadatas[project_name] = ProjectMetadata(
                 project_name,
-                cls.build_cmds[project_name],
+                self.build_cmds[project_name],
                 ["make install"],
                 ["make clean"],
                 project_url=f"https://github.com/{project}",
-                commit_sha=cls.commit_shas[project_name])
+                commit_sha=self.commit_shas[project_name])
 
-    def insert_metadata(cls):
-        for project_metadata in cls.metadatas.values():
+    def insert_metadata(self):
+        """
+        Insert metadata into metadata storage.
+        """
+        for project_metadata in self.metadatas.values():
             print(project_metadata)
-            cls.metadata_storage.insert(project_metadata)
+            self.metadata_storage.insert(project_metadata)
 
-    def init_projects(cls):
-        for project_name in cls.metadatas.keys():
-            cls.projects[project_name] = ProjectRepo(
-                cls.repo_paths[project_name],
-                cls.commit_shas[project_name],
-                cls.metadata_storage,
+    def init_projects(self):
+        """
+        Initialize Projects.
+        """
+        for project_name in self.metadatas.keys():
+            self.projects[project_name] = ProjectRepo(
+                self.repo_paths[project_name],
+                self.commit_shas[project_name],
+                self.metadata_storage,
                 num_cores=8,
                 sentence_extraction_method=SentenceExtractionMethod.HEURISTIC)
 
@@ -93,20 +108,20 @@ class TestLooper(unittest.TestCase):
     """
 
     @classmethod
-    def setUpClass(cls):
+    def setUpClass(self):
         """
         Use the base constructor, with some additions.
         """
-        cls.tester = DatasetFactory()
+        self.tester = DatasetFactory()
 
     @classmethod
-    def tearDownClass(cls):
+    def tearDownClass(self):
         """
         Remove the cloned repos.
         """
-        for project_name, repo in cls.tester.repos.items():
+        for project_name, repo in self.tester.repos.items():
             del repo
-            shutil.rmtree(os.path.join(cls.tester.repo_paths[project_name]))
+            shutil.rmtree(os.path.join(self.tester.repo_paths[project_name]))
 
     def test_looper(self):
         """
