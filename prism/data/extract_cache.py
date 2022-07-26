0950cd903d47b663f8651016143b9928735cdafd
Maxwell Reeser <maxwell.reeser@radiancetech.com>
2022-07-26 14:54:36 +0000

    Addressing comments

--- prism/data/extract_cache.py
+++ prism/data/extract_cache.py
@@ -1,7 +1,7 @@
 """
 Module for storing cache extraction functions.
 """
-from typing import Set
+from typing import Set, Callable
 
 from prism.data.build_cache import (
     CoqProjectBuildCache,
@@ -15,48 +15,27 @@ from prism.project.repo import ProjectRepo
 from prism.util.opam import OpamAPI
 
 
+def get_switch(metadata : ProjectMetadata) -> OpamSwitch:
+    return OpamAPI.find_switch(metadata)
+
 def extract_cache(
         coq_version: str,
         build_cache: CoqProjectBuildCache,
         project: ProjectRepo,
-        commit_sha: str) -> None:
+        commit_sha: str,
+        process_file: Callable) -> None:
     """
     Extract cache from project commit and insert into build_cache.
     """
-    OpamAPI.set_switch(metadata=project.metadata)
-    if commit_sha not in build_cache:
+    project.git.checkout(commit_sha)
+    project.opam_switch = get_switch(project.metadata)
+    coq_version = project.metadata.coq_version
+    if (project.name, commit_sha, coq_version) not in build_cache:
         try:
-            project.git.checkout(commit_sha)
             project.build()
             command_data = {}
             for filename in project.get_file_list():
-                file_commands: Set[VernacCommandData] = command_data.setdefault(
-                    filename,
-                    set())
-                doc = project.get_file(filename)
-                beg_char_idx = 0
-                end_char_idx = 0
-                for (sentence_idx,
-                     sentence) in enumerate(project.extract_sentences(
-                         doc,
-                         sentence_extraction_method=project
-                         .sentence_extraction_method)):
-                    end_char_idx += len(sentence)
-                    command_type, identifier = ParserUtils.extract_identifier(sentence)
-                    file_commands.add(
-                        VernacCommandData(
-                            identifier,
-                            command_type,
-                            SexpInfo.Loc(
-                                filename,
-                                sentence_idx,
-                                0,
-                                sentence_idx,
-                                0,
-                                beg_char_idx,
-                                end_char_idx),
-                            None))
-                    beg_char_idx = end_char_idx
+                command_data[filename] = process_file(project, filename)
             data = ProjectCommitData(project.metadata, command_data)
             build_cache.insert(data)
         except ProjectBuildError as pbe:
