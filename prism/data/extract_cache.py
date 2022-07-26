3ab1e414a6d103a95f0c47549f57b218b0d6c968
Maxwell Reeser <maxwell.reeser@radiancetech.com>
2022-07-26 15:40:57 +0000

    Adding back in and returning active switch

--- prism/data/extract_cache.py
+++ prism/data/extract_cache.py
@@ -14,7 +14,7 @@ def get_switch(metadata: ProjectMetadata) -> OpamSwitch:
     """
     Pass args for use as a stub.
     """
-    return OpamAPI.find_switch(metadata)
+    return OpamAPI.active_switch
 
 
 def extract_cache(
@@ -22,7 +22,7 @@ def extract_cache(
         build_cache: CoqProjectBuildCache,
         project: ProjectRepo,
         commit_sha: str,
-        process_file: Callable) -> None:
+        process_project: Callable) -> None:
     """
     Extract cache from project commit and insert into build_cache.
     """
@@ -31,11 +31,37 @@ def extract_cache(
     coq_version = project.metadata.coq_version
     if (project.name, commit_sha, coq_version) not in build_cache:
         try:
-            project.build()
             command_data = {}
             for filename in project.get_file_list():
-                command_data[filename] = process_file(project, filename)
+                file_commands: Set[VernacCommandData] = command_data.setdefault(
+                    filename,
+                    set())
+                doc = project.get_file(filename)
+                beg_char_idx = 0
+                end_char_idx = 0
+                for (sentence_idx,
+                     sentence) in enumerate(project.extract_sentences(
+                         doc,
+                         sentence_extraction_method=project
+                         .sentence_extraction_method)):
+                    end_char_idx += len(sentence)
+                    command_type, identifier = ParserUtils.extract_identifier(sentence)
+                    file_commands.add(
+                        VernacCommandData(
+                            identifier,
+                            command_type,
+                            SexpInfo.Loc(
+                                filename,
+                                sentence_idx,
+                                0,
+                                sentence_idx,
+                                0,
+                                beg_char_idx,
+                                end_char_idx),
+                            None))
+                    beg_char_idx = end_char_idx
             data = ProjectCommitData(project.metadata, command_data)
             build_cache.insert(data)
         except ProjectBuildError as pbe:
             print(pbe.args)
+            process_project(project)
