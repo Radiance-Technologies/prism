6d6967ad77a91d9b73ae1e40a618ad781ac4411c
Maxwell Reeser <maxwell.reeser@radiancetech.com>
2022-08-22 16:12:22 +0000

    Maybe it will actually fix it?

--- prism/util/build_tools/tests/test_coqdep.py
+++ prism/util/build_tools/tests/test_coqdep.py
@@ -65,19 +65,15 @@ class TestOpamCoqDepAPI(unittest.TestCase):
             'Reduction.vo': ['Redexes.vo',
                              'Marks.vo',
                              'Substitution.vo'],
-            'Terms.vo':
-                ['Reduction.vo',
-                 'Redexes.vo',
-                 'Marks.vo',
-                 'Substitution.vo'],
-            'Test.vo':
-                [
-                    'Terms.vo',
-                    'Reduction.vo',
-                    'Redexes.vo',
-                    'Substitution.vo',
-                    'Marks.vo'
-                ]
+            'Terms.vo': ['Reduction.vo',
+                         'Redexes.vo',
+                         'Marks.vo',
+                         'Substitution.vo'],
+            'Test.vo': ['Terms.vo',
+                        'Reduction.vo',
+                        'Redexes.vo',
+                        'Substitution.vo',
+                        'Marks.vo']
         }
         expected = nx.DiGraph(edges)
 
