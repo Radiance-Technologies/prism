6a7316671c1137966e5a496c2f4d21ce61660127
Andrew Gardner <Andrew.Gardner@radiancetech.com>
2022-08-30 16:44:38 +0000

    Make custom exception types pickleable.

--- prism/language/gallina/exception.py
+++ prism/language/gallina/exception.py
@@ -5,6 +5,8 @@ Adapted from `roosterize.parser.SexpAnalyzer`
 at https://github.com/EngineeringSoftware/roosterize/.
 """
 
+from typing import Tuple, Union
+
 from prism.language.sexp.node import SexpNode
 
 
@@ -13,14 +15,12 @@ class SexpAnalyzingException(Exception):
     For representing errors thrown during Gallina s-expression analysis.
     """
 
-    def __init__(
-            self,
-            sexp: SexpNode = None,
-            message: str = "",
-            *args,
-            **kwargs):
+    def __init__(self, sexp: SexpNode, message: str = ""):
         self.sexp = sexp
         self.message = message
+
+    def __reduce__(self) -> Union[str, Tuple[SexpNode, str]]:  # noqa: D105
+        return SexpAnalyzingException, (self.sexp, self.message)
 
     def __str__(self):  # noqa: D105
         return f"{self.message}\nin sexp: {self.sexp.pretty_format()}"
