818c6c64e14803e16f8ceed9c3cd3a36e5ef6c64
Wesley Henderson <robert.henderson@radiancetech.com>
2022-09-14 17:31:18 -0500

    Moved functions from build_default_commits into extract_cache module

--- prism/data/extract_cache.py
+++ prism/data/extract_cache.py
@@ -1,8 +1,15 @@
 """
 Module for storing cache extraction functions.
 """
-from functools import reduce
-from typing import Callable, List, Optional, Union
+import logging
+import traceback
+from functools import partial, reduce
+from multiprocessing import Pool
+from pathlib import Path
+from typing import Callable, Dict, List, Optional, Set, Union
+
+import tqdm
+from seutil import io
 
 from prism.data.build_cache import (
     CoqProjectBuildCache,
@@ -12,16 +19,20 @@ from prism.data.build_cache import (
     VernacCommandData,
     VernacDict,
 )
+from prism.data.commit_map import Except, ProjectCommitUpdateMapper
+from prism.data.setup import create_default_switches
 from prism.interface.coq.serapi import SerAPI
 from prism.language.heuristic.util import ParserUtils
-from prism.project.base import SEM, Project
+from prism.project.base import SEM, Project, SentenceExtractionMethod
 from prism.project.exception import ProjectBuildError
+from prism.project.metadata.storage import MetadataStorage
 from prism.project.repo import ProjectRepo
 from prism.util.opam import PackageFormula
 from prism.util.opam.formula import LogicalPF, LogOp
 from prism.util.opam.version import Version
 from prism.util.radpytools.os import pushd
 from prism.util.swim import SwitchManager
+from prism.util.swim.auto import AutoSwitchManager
 
 from ..language.gallina.analyze import SexpAnalyzer, SexpInfo
 
@@ -237,3 +248,170 @@ def extract_cache_new(
     # release the switch
     switch_manager.release_switch(project.opam_switch)
     project.opam_switch = original_switch
+
+
+class CacheExtractor:
+    """
+    Class for managing a broad Coq project cache extraction process.
+    """
+
+    @staticmethod
+    def get_project(
+            root_path: str,
+            metadata_storage: MetadataStorage,
+            project_name: str) -> ProjectRepo:
+        """
+        Get the identified project's `ProjectRepo` representation.
+        """
+        repo_path = Path(root_path) / project_name
+        return ProjectRepo(
+            repo_path,
+            metadata_storage,
+            sentence_extraction_method=SentenceExtractionMethod.SERAPI)
+
+    @staticmethod
+    def get_project_func(  # noqa: D103, D102
+            root_path: str,
+            metadata_storage: MetadataStorage) -> Callable[[str],
+                                                           ProjectRepo]:
+        from seutil import io
+        io.dump()
+        return partial(CacheExtractor.get_project, root_path, metadata_storage)
+
+    @staticmethod
+    def get_commit_iterator(
+            default_commits: Dict[str,
+                                  List[str]],
+            project: ProjectRepo) -> Set[str]:
+        """
+        Get an iterator over a project's default commits.
+        """
+        return default_commits[project.metadata.project_name]
+
+    @staticmethod
+    def get_commit_iterator_func(  # noqa: D103, D102
+        default_commits: Dict[str,
+                              List[str]]) -> Callable[[ProjectRepo],
+                                                      List[str]]:
+        return partial(CacheExtractor.get_commit_iterator, default_commits)
+
+    @staticmethod
+    def process_commit(
+            switch_manager: SwitchManager,
+            project: ProjectRepo,
+            commit: str,
+            results: None) -> None:
+        """
+        Build the project at the given commit.
+        """
+        try:
+            project.git.checkout(commit)
+            coq_version = project.metadata_storage.get_project_coq_versions(
+                project.name,
+                project.remote_url,
+                project.commit_sha)
+            try:
+                coq_version = coq_version.pop()
+            except KeyError:
+                coq_version = '8.10.2'
+            print(f'Choosing "coq.{coq_version}" for {project.name}')
+            # get a switch
+            project.infer_opam_dependencies()  # force inference
+            dependency_formula = get_dependency_formula(
+                project.opam_dependencies,
+                project.ocaml_version,
+                coq_version)
+            original_switch = project.opam_switch
+            project.opam_switch = switch_manager.get_switch(
+                dependency_formula,
+                variables={
+                    'build': True,
+                    'post': True,
+                    'dev': True
+                })
+            # process the commit
+            _ = project.build()
+        except Exception:
+            logging.debug(
+                f"Skipping build for {project.metadata.project_name}:"
+                f"{traceback.format_exc()}")
+            raise
+        finally:
+            switch_manager.release_switch(project.opam_switch)
+            project.opam_switch = original_switch
+
+    @staticmethod
+    def get_process_commit_func(  # noqa: D103, D102
+        switch_manager: SwitchManager) -> Callable[[ProjectRepo,
+                                                    str,
+                                                    None],
+                                                   None]:
+        return partial(CacheExtractor.process_commit, switch_manager)
+
+    @staticmethod
+    def main(
+            root_path: str,
+            storage_path: str,
+            default_commits_path: str) -> None:
+        """
+        Build all projects at `root_path` and save updated metadata.
+
+        Parameters
+        ----------
+        root_path : PathLike
+            The root directory containing each project's directory.
+            The project directories do not need to already exist.
+        storage_path : PathLike
+            The path to a file containing metadata for each project to
+            be built at `root_path`.
+        default_commits_path : PathLike
+            The path to a file identifying the default commits for each
+            project in the storage.
+        """
+        # Initialize from arguments
+        metadata_storage = MetadataStorage.load(storage_path)
+        default_commits: Dict[str,
+                              List[str]] = io.load(
+                                  default_commits_path,
+                                  clz=dict)
+        create_default_switches(7)
+        switch_manager = AutoSwitchManager()
+        # Generate list of projects
+        projects = list(
+            tqdm.tqdm(
+                Pool(20).imap(
+                    CacheExtractor.get_project_func(
+                        root_path,
+                        metadata_storage),
+                    metadata_storage.projects),
+                desc="Initializing Project instances",
+                total=len(metadata_storage.projects)))
+        # Create commit mapper
+        project_looper = ProjectCommitUpdateMapper(
+            projects,
+            CacheExtractor.get_commit_iterator_func(default_commits),
+            CacheExtractor.get_process_commit_func(switch_manager),
+            "Building projects",
+            terminate_on_except=False)
+        # Build projects in parallel
+        results, metadata_storage = project_looper.update_map(30)
+        storage_dir = Path(storage_path).parent
+        # report errors
+        with open(storage_dir / "build_error_log.txt") as f:
+            for p, result in results.items():
+                if isinstance(result, Except):
+                    print(
+                        f"{type(result.exception)} encountered in project {p}:")
+                    print(result.trace)
+                    f.write(
+                        '\n'.join(
+                            [
+                                "###################################################",
+                                f"{type(result.exception)} encountered in project {p}:",
+                                result.trace
+                            ]))
+        # update metadata
+        metadata_storage.dump(
+            metadata_storage,
+            storage_dir / "updated_metadata.yaml")
+        print("Done")
--- scripts/data/project_caching.py
+++ scripts/data/project_caching.py
@@ -1,3 +1,9 @@
 """
 Project-caching script.
 """
+# Keep enumeration of projects to begin with
+# Overall call to project commit iterator
+# Error logging
+
+# Replace core functionality with extract cache stuff
+# Project commit iterator will need updating
