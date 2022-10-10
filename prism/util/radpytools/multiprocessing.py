a0f6c83e9bf84b83cdfabdbea1e68dc474bf29cb
Andrew Gardner <Andrew.Gardner@radiancetech.com>
2022-10-10 19:40:45 +0000

    Add synchronizedproperty.

--- prism/util/radpytools/multiprocessing.py
+++ prism/util/radpytools/multiprocessing.py
@@ -6,6 +6,8 @@ from multiprocessing import RLock
 from multiprocessing.synchronize import SemLock
 from typing import Any, Callable, Dict, Optional, Type, TypeVar
 
+from prism.util.radpytools import descriptor
+
 S = TypeVar('S')
 T = TypeVar('T')
 
@@ -37,7 +39,7 @@ def critical(f: Callable[[T],
     return inner
 
 
-class _synchronizedmethod:
+class _synchronizedmethod(descriptor):
     """
     Internal implementation of synchronized methods.
 
@@ -62,14 +64,9 @@ class _synchronizedmethod:
                                   SemLock] = RLock,
             **kwargs: Dict[str,
                            Any]) -> None:
-        if not callable(func) and not hasattr(func, "__get__"):
-            raise TypeError(f"{repr(func)} is not callable or a descriptor")
-        self._f = func
-        self._isclassmethod = isinstance(func, (classmethod, staticmethod))
-        self._f_name = None
+        super().__init__(func, require_read=True)
         self.semlock_name = semlock_name
         self.sync_name = None
-        self.__doc__ = func.__doc__
         self.lock = RLock()
         self._semlock_cls = semlock_cls
         self._semlock_kwargs = kwargs
@@ -91,40 +88,32 @@ class _synchronizedmethod:
         TypeError
             If one attempts to alter the name of the descriptor.
         """
-        if self._f_name is None:
-            self._f_name = name
-            self._owner = owner
-        elif name != self._f_name:
-            raise TypeError(
-                "Cannot assign the same mutexmethod to two different names "
-                f"({self._f_name!r} and {name!r}).")
-        if self._owner is None:
-            self._owner = owner
-        elif owner != self._owner:
-            raise TypeError(
-                "Cannot assign the same mutexmethod to two different types "
-                f"({self._owner!r} and {owner!r}).")
+        super().__set_name__(owner, name)
         if self.semlock_name is None:
             self.semlock_name = self.get_semlock_name(name, owner)
         self.sync_name = self.get_sync_name(name, owner)
 
-    def __get__(self, instance: T, owner: Type[T] = None):  # noqa: C901
+    def __get__(self,  # noqa: C901
+                instance: T,
+                owner: Type[T] = None) -> Callable[...,
+                                                   Any]:
         """
-        Retrieve the mutex method wrapper.
+        Retrieve the synchronized method wrapper.
 
         Each instance of the class is assigned its own lock.
 
         Parameters
         ----------
         instance : T
-            An instance of the mutex method owner.
+            An instance of the synchronized method owner.
         owner : type, optional
             The type of the instance, by default None.
 
         Returns
         -------
         callable
-            The instance method wrapped with a mutex mechanism.
+            The instance method wrapped with a synchronization
+            mechanism.
 
         Notes
         -----
@@ -142,13 +131,13 @@ class _synchronizedmethod:
             owner = type(instance)
         if obj is not None:
             try:
-                return getattr(obj, self.sync_name)
+                result = getattr(obj, self.sync_name)
             except AttributeError:
                 with self.lock:
                     # check to see if another thread created the wrapper
                     # while we waited for this lock
                     try:
-                        return getattr(obj, self.sync_name)
+                        result = getattr(obj, self.sync_name)
                     except AttributeError:
                         # Create the synchronization wrapper.
                         # Get semlock used for synchronization
@@ -180,15 +169,24 @@ class _synchronizedmethod:
                                 f"{self.semlock_name} is not a supported"
                                 f"synchronization primitive (got {semlock!r})")
                         # create and store synchronization wrapper
-                        wrapper = critical(
-                            self._f.__get__(instance,
-                                            owner),
-                            semlock)
+                        if self._isproperty:
+                            wrapper = critical(
+                                lambda: self._f.__get__(instance,
+                                                        owner),
+                                semlock)
+                        else:
+                            wrapper = critical(
+                                self._f.__get__(instance,
+                                                owner),
+                                semlock)
                         if self._isclassmethod:
                             type.__setattr__(obj, self.sync_name, wrapper)
                         else:
                             object.__setattr__(obj, self.sync_name, wrapper)
-                        return wrapper
+                        result = wrapper
+            if self._isproperty:
+                result = result()
+            return result
         else:
             # obj can only be None if a class tried to call an instance
             # method
@@ -316,3 +314,68 @@ def synchronizedmethod(
 
     # called without parentheses
     return wrap(_func)
+
+
+def synchronizedproperty(
+        _func: Callable[...,
+                        Any] = None,
+        *,
+        semlock_name: Optional[str] = None,
+        semlock_cls: Callable[...,
+                              SemLock] = RLock,
+        **kwargs: Dict[str,
+                       Any]) -> _synchronizedmethod:
+    """
+    Make a synchronized property decorator.
+
+    A wrapper around a method that synchronizes its execution with a
+    semaphore/lock and behaves outwardly as a property.
+    The lock may be created on a per-method basis (the default) or
+    grouped with other methods according to a provided name.
+
+    Parameters
+    ----------
+    _func : callable
+        The method to be wrapped with a caching mechanism.
+        The arguments to `f` must be hashable.
+        This argument should not be called by keyword and must be
+        provided positionally.
+    semlock_name : str or None, optional
+        The name of the semaphore/lock
+    semlock_cls : Callable[..., SemLock], optional
+        Nominally, the type of synchronization primitive to associate
+        with `semlock_name`.
+        In practice, a function that takes an argument and returns an
+        instance of `SemLock`.
+        By default, `RLock` is assumed.
+    kwargs
+        Additional keyword arguments to `semlock_cls` (e.g., the
+        initial value for a semaphore).
+
+    Returns
+    -------
+    _synchronizedmethod
+        Internal implementation of a synchronized method.
+
+    Examples
+    --------
+    >>> class Example:
+    ...     xs : List[int] = []
+    ...
+    ...     @synchronizedproperty
+    ...     def f(self) -> List[int]:
+    ...         for y in range(5):
+    ...             self.xs.append(y)
+    ...         return list(self.xs)
+    ...
+    >>> ex = Example()
+    >>> tasks = [(), ()]
+    >>> with multiprocessing.pool.ThreadPool(2) as pool:
+    ...     pool.map(ex.f, tasks)
+    [[0, 1, 2, 3, 4], [0, 1, 2, 3, 4, 0, 1, 2, 3, 4]]
+    """
+    return synchronizedmethod(
+        property(_func),
+        semlock_name=semlock_name,
+        semlock_cls=semlock_cls,
+        **kwargs)
