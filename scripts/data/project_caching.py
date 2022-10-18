8742e1276987faa30a991e739181fec86c647581
Andrew Gardner <Andrew.Gardner@radiancetech.com>
2022-10-18 19:27:33 +0000

    Remove redundant project_caching.py script

--- scripts/data/project_caching.py
+++
@@ -1,9 +0,0 @@
-"""
-Project-caching script.
-"""
-# Keep enumeration of projects to begin with
-# Overall call to project commit iterator
-# Error logging
-
-# Replace core functionality with extract cache stuff
-# Project commit iterator will need updating
