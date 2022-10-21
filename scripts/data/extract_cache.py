000ec23bd175f7b13edc24748c1f27a2a61a730a
Andrew Gardner <Andrew.Gardner@radiancetech.com>
2022-10-21 16:44:26 +0000

    Miscellaneous changes to scripts/extract_cache.py:
    * Add types to str, int args
    * Create log directory if it does not exist
    * Add --coq-versions argument
    * Use pathlib.Path to more robustly create default args (avoid double slashes)

--- scripts/data/extract_cache.py
+++ scripts/data/extract_cache.py
@@ -3,9 +3,8 @@ Script to perform cache extraction.
 """
 import argparse
 import logging
-import os
-import pathlib
 from datetime import datetime
+from pathlib import Path
 from typing import Optional
 
 from prism.data.extract_cache import (
@@ -21,10 +20,16 @@ from prism.util.swim import (
 
 if __name__ == "__main__":
     # Get args
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description="Extract Coq Proof Assistant state line-by-linea cross a range of "
+        "project repositories, commits, and Coq versions.\n"
+        "Note that commits are currently limited to those created in 2019 "
+        "or later; use a different commit iterator if other behavior is "
+        "desired. ")
     parser.add_argument(
         "--root",
-        default=str(pathlib.Path.home() / "projects" / "PEARLS"),
+        default=str(Path.home() / "projects" / "PEARLS"),
+        type=str,
         help="Root directory for extraction. If your directory structure is"
         " arranged in a certain way, you can provide this argument and"
         " otherwise ignore '--default-commits-path', '--cache-dir',"
@@ -33,38 +38,45 @@ if __name__ == "__main__":
         " expected. This argument is effectively ignored if all other"
         " paths are provided.")
     args, _ = parser.parse_known_args()
-    ROOT: str = args.root
+    ROOT = Path(args.root)
     parser.add_argument(
         "--default-commits-path",
-        default=f"{ROOT}/prism/pearls/dataset/default_commits.yml",
+        default=str(ROOT / "prism/pearls/dataset/default_commits.yml"),
+        type=str,
         help="Path to a yaml file containing default commits for each project."
         " Each key should be a project name, and each value should be a"
         " list of default commits. The first item is used as the default."
         " If no commits are provided for a project, it is ignored.")
     parser.add_argument(
         "--cache-dir",
-        default=f"{ROOT}/caching",
+        default=str(ROOT / "build_cache"),
+        type=str,
         help="The directory to read cache from and write new cache to.")
     parser.add_argument(
         "--mds-file",
-        default=f"{ROOT}/prism/pearls/dataset/agg_coq_repos.yml",
+        default=str(ROOT / "prism/pearls/dataset/agg_coq_repos.yml"),
+        type=str,
         help="The storage file to load metadata from for these projects.")
     parser.add_argument(
         "--project-root-path",
-        default=f"{ROOT}/repos_full",
+        default=str(ROOT / "repos_full"),
+        type=str,
         help="The path to the project root directory, where project repos"
         " either already exist or will be cloned into.")
     parser.add_argument(
         "--log-dir",
-        default=f"{ROOT}/caching/log",
+        default=str(ROOT / "caching/log"),
+        type=str,
         help="Directory to store log files in.")
     parser.add_argument(
         "--extract-nprocs",
         default=8,
+        type=int,
         help="Number of concurrent workers to allow for extraction.")
     parser.add_argument(
         "--n-build-workers",
         default=1,
+        type=int,
         help="Number of workers to allow for building projects, per project.")
     parser.add_argument(
         "--force-serial",
@@ -75,6 +87,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--num-switches",
         default=7,
+        type=int,
         help="Number of processes used to set up the set of default Opam"
         " switches.")
     parser.add_argument(
@@ -87,11 +100,20 @@ if __name__ == "__main__":
     parser.add_argument(
         "--max-num-commits",
         default=None,
+        type=int,
         help="If provided, the number of commits per project is capped at"
         " this number.")
     parser.add_argument(
+        "--coq-versions",
+        nargs="*",
+        default=[],
+        help="A list of Coq versions that will be considered for each commit."
+        " If not provided, then only Coq 8.10[.2] is considered."
+        " Currently, only Coq 8.9.X to 8.15.Y are supported.")
+    parser.add_argument(
         "--updated-md-storage-file",
         default=None,
+        type=str,
         help="If provided, the metadata storage, which may be updated as cache"
         " is extracted, will be saved to the file given. If not provided, the"
         " --mds-file will be updated.")
@@ -100,25 +122,28 @@ if __name__ == "__main__":
     cache_dir: str = args.cache_dir
     mds_file: str = args.mds_file
     project_root_path: str = args.project_root_path
-    log_dir: str = args.log_dir
+    log_dir = Path(args.log_dir)
     extract_nprocs: int = int(args.extract_nprocs)
     n_build_workers: int = int(args.n_build_workers)
     force_serial: bool = bool(args.force_serial)
     num_switches: int = int(args.num_switches)
     project_names = args.project_names if args.project_names else None
+    coq_versions = args.coq_versions if args.coq_versions else None
     max_num_commits: Optional[int] = int(args.max_num_commits)
     if args.updated_md_storage_file:
-        updated_md_storage_file: str = args.updated_md_storage_file
+        updated_md_storage_file = args.updated_md_storage_file
     else:
-        updated_md_storage_file: str = mds_file
-    if updated_md_storage_file:
-        os.makedirs(pathlib.Path(updated_md_storage_file).parent, exist_ok=True)
+        updated_md_storage_file = mds_file
+    updated_md_storage_file = Path(updated_md_storage_file)
+    updated_md_storage_file.parent.mkdir(parents=True, exist_ok=True)
     # Force redirect the root logger to a file
     # This might break due to multiprocessing. If so, it should just
     # be disabled
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     log_file_name = f"extraction_log_{timestamp}.log"
-    log_file_path = os.path.join(log_dir, log_file_name)
+    log_file_path = log_dir / log_file_name
+    if not log_dir.exists():
+        log_dir.mkdir(parents=True, exist_ok=True)
     logging.basicConfig(filename=log_file_path, force=True)
     # Do things
     create_default_switches(num_switches)
@@ -132,7 +157,9 @@ if __name__ == "__main__":
         mds_file,
         swim,
         default_commits_path,
-        cache_extract_commit_iterator)
+        cache_extract_commit_iterator,
+        coq_version_iterator=lambda _,
+        __: coq_versions)
     cache_extractor.run(
         project_root_path,
         log_dir,
