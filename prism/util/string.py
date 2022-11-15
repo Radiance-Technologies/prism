ba31bc04fc6536af5c6841d15129708a471a5206
Andrew Gardner <Andrew.Gardner@radiancetech.com>
2022-11-15 22:55:01 +0000

    Add quote_escape; escape more special characters than just backslash and double quote

--- prism/util/string.py
+++ prism/util/string.py
@@ -4,27 +4,29 @@ Miscellaneous utilities for SerAPI interaction.
 
 import re
 
+from prism.util.iterable import CallableIterator
+
 _whitespace_regex = re.compile(r"\s+", flags=re.DOTALL)
+_escape_regex = re.compile('(["\'\\\b\f\t\n\r\v\a])')
 
 
-def escape(cmd: str) -> str:
+def escape(s: str) -> str:
     """
-    Sanitize the given command by escaping special characters.
-
-    In particular, backslashes and double quotes are escaped by
-    prepending backslashes to each.
+    Sanitize the given string by escaping special characters.
 
     Parameters
     ----------
-    cmd : str
-        A command.
+    s : str
+        A string.
 
     Returns
     -------
     str
         The sanitized command.
     """
-    return cmd.replace("\\", r"\\").replace('"', r'\"')
+    matches = _escape_regex.finditer(s)
+    replacements = [rf"\{m[0]}" for m in matches]
+    return _escape_regex.sub(CallableIterator(replacements), s)
 
 
 def normalize_spaces(s: str) -> str:
@@ -42,4 +44,33 @@ def unquote(s: str) -> str:
     """
     if s.startswith('"') and s.endswith('"'):
         s = s[1 :-1]
+    return s
+
+
+def quote_escape(s: str, quotes_only: bool = False) -> str:
+    """
+    Escape the given string and surround it in double quotes.
+
+    Quotes are only added if any character in the string needed to be
+    escaped.
+
+    Parameters
+    ----------
+    s : str
+        A string.
+    quotes_only : bool, optional
+        Escape only double quotes, by default False
+
+    Returns
+    -------
+    str
+        The escaped and quoted string.
+    """
+    if _escape_regex.search(s) is not None:
+        # escape double quotes only
+        if quotes_only:
+            escaped = s.replace('"', r'\"')
+        else:
+            escaped = escape(s)
+        s = f'"{escaped}"'
     return s
