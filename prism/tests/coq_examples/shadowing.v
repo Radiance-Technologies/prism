40e21affb26e24cf358f3caa1ac9a8d409bccabc
Andrew Gardner <Andrew.Gardner@radiancetech.com>
2022-11-18 02:48:17 +0000

    Verify that identifiers containing escaped characters get expanded

--- prism/tests/coq_examples/shadowing.v
+++ prism/tests/coq_examples/shadowing.v
@@ -4,8 +4,8 @@
 Shadow a  global definition.
 Also serves as edge case for a definition referencing what it shadows.
 *)
-Fixpoint nat (n m:nat) {struct n} : nat :=
-match n with
+Fixpoint nat (n' m:nat) {struct n'} : nat :=
+match n' with
 | O => m
 | S p => S (nat p m)
 end.
