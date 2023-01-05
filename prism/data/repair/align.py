b9c50230d253c18e478ecfc0896af30540842d5a
Andrew Gardner <Andrew.Gardner@radiancetech.com>
2023-01-05 16:55:46 +0000

    Add parametric, diff-constrained align_commits

--- prism/data/repair/align.py
+++ prism/data/repair/align.py
@@ -4,13 +4,16 @@ Tools for aligning the same proofs across commits.
 
 import enum
 import math
-from typing import Callable, List, Optional, TypeVar
+from bisect import bisect_left
+from typing import Callable, Dict, List, Optional, TypeVar
 
 import numpy as np
 from leven import levenshtein
 
 from prism.data.build_cache import ProjectCommitData, VernacSentence
+from prism.data.repair.diff import commands_in_diff
 from prism.util.alignment import Alignment, lazy_align
+from prism.util.diff import GitDiff
 
 T = TypeVar('T')
 
@@ -226,4 +229,209 @@ def align_commits_per_file(
         aligned_sentences += np.array([a_offset, b_offset])
         aligned_files.append(aligned_sentences)
     alignment = np.concatenate(aligned_files, axis=0)
+    return alignment
+
+
+def _compute_diff_alignment(
+    a: ProjectCommitData,
+    b: ProjectCommitData,
+    diff: GitDiff,
+    a_indices_in_diff: Dict[str,
+                            List[int]],
+    b_indices_in_diff: Dict[str,
+                            List[int]],
+    align: Callable[[ProjectCommitData,
+                     ProjectCommitData],
+                    np.ndarray]
+) -> np.ndarray:
+    """
+    Get the alignment only between items contained in a Git diff.
+
+    Parameters
+    ----------
+    a, b : ProjectCommitData
+        Command data extracted from two commits of a project.
+    diff : GitDiff
+        A precomputed Git diff between the commits of `a` and `b` with
+        `a` taken to be "before" the change.
+    a_indices_in_diff : Dict[str, List[int]]
+        A precomputed set of per-file command indices within `a` that
+        intersect the `diff`.
+    b_indices_in_diff : Dict[str, List[int]]
+        A precomputed set of per-file command indices within `b` that
+        intersect the `diff`.
+    align : Callable[[ProjectCommitData, ProjectCommitData], np.ndarray]
+        An alignment algorithm that will be applied to commands that
+        appear in the provided `diff`.
+
+    Returns
+    -------
+    np.ndarray
+        An array of pairs of integers indicating the indices of aligned
+        commands between each commit with commands enumerated over all
+        commands appearing within the diff in files in `a` and `b`.
+        For example, ``(1,1)`` matches the first element of the first
+        file of `a` that appears in the `diff` to the first element of
+        the first file of `b` that appears in the `diff`.
+        Indices of elements in either `a` or `b` that were skipped in
+        the alignment do not appear in the output.
+    """
+    # downselect commit data to changed commands
+    a_in_diff = ProjectCommitData(
+        a.project_metadata,
+        {
+            k: [v[i] for i in a_indices_in_diff[k]] for k,
+            v in a.command_data.items()
+        },
+        a.file_dependencies,
+        a.environment,
+        a.build_result)
+    b_in_diff = ProjectCommitData(
+        b.project_metadata,
+        {
+            k: [v[i] for i in b_indices_in_diff[k]] for k,
+            v in b.command_data.items()
+        },
+        b.file_dependencies,
+        b.environment,
+        b.build_result)
+    # rename files in b to match those renamed in a
+    rename_map: Dict[str,
+                     str] = {k: k for k in b.command_data.keys()}
+    for change in diff.changes:
+        if change.is_rename:
+            rename_map[change.after_filename] = change.before_filename
+    b_in_diff.command_data = {
+        rename_map[k]: v for k,
+        v in b_in_diff.command_data.items()
+    }
+    if b_in_diff.file_dependencies is not None:
+        b_in_diff.file_dependencies = {
+            rename_map[k]: [rename_map[f] for f in v] for k,
+            v in b_in_diff.file_dependencies.items()
+        }
+    assert b_in_diff.files == [rename_map[f] for f in b.files]
+    # calculate alignment only for those items that are known to have
+    # changed
+    diff_alignment = align(a_in_diff, b_in_diff)
+    return diff_alignment
+
+
+def align_commits(
+    a: ProjectCommitData,
+    b: ProjectCommitData,
+    diff: GitDiff,
+    align: Callable[[ProjectCommitData,
+                     ProjectCommitData],
+                    np.ndarray]
+) -> np.ndarray:
+    """
+    Align two `ProjectCommit` based on the provided alignment algorithm.
+
+    Parameters
+    ----------
+    a, b : ProjectCommitData
+        Command data extracted from two commits of a project.
+    diff : GitDiff
+        A precomputed diff between the commits represented by `a` and
+        `b`.
+    align : Callable[[ProjectCommitData, ProjectCommitData], np.ndarray]
+        An alignment algorithm that will be applied to commands that
+        appear in the provided `diff`.
+
+    Returns
+    -------
+    np.ndarray
+        An array of pairs of integers indicating the indices of aligned
+        commands between each commit with commands enumerated over all
+        files in `a` and `b`.
+        For example, ``(1,1)`` matches the first element of the first
+        file of `a` to the first element of the first file of `b`.
+        Indices of elements in either `a` or `b` that were skipped in
+        the alignment do not appear in the output.
+    """
+    # get changed and unchanged sentence per-file indices
+    a_indices_in_diff = commands_in_diff(a, diff, True)
+    b_indices_in_diff = commands_in_diff(b, diff, False)
+    a_files = a.files
+    b_files = b.files
+    a_file_sizes = {k: len(v) for k,
+                    v in a.command_data.items()}
+    b_file_sizes = {k: len(v) for k,
+                    v in b.command_data.items()}
+    a_indices_not_in_diff = {
+        k: [i for i in range(a_file_sizes[k]) if bisect_left(v,
+                                                             i) == len(v)]
+        for k,
+        v in a_indices_in_diff.items()
+    }
+    b_indices_not_in_diff = {
+        k: [i for i in range(b_file_sizes[k]) if bisect_left(v,
+                                                             i) == len(v)]
+        for k,
+        v in b_indices_in_diff.items()
+    }
+    # calculate alignment only for those items that are known to have
+    # changed
+    diff_alignment = _compute_diff_alignment(
+        a,
+        b,
+        diff,
+        a_indices_in_diff,
+        b_indices_in_diff,
+        align)
+    # offset file-level indices to match final global alignment
+    a_file_offsets = dict(
+        zip(a_files,
+            [0] + list(np.cumsum([a_file_sizes[x] for x in a_files]))))
+    b_file_offsets = dict(
+        zip(
+            b_files,
+            [0] + list(np.cumsum([len(b.command_data[x]) for x in b_files]))))
+    # get final indices of changed elements of a
+    changed_a_indices = np.concatenate(
+        [
+            np.asarray(v) + a_file_offsets[k] for k,
+            v in a_indices_in_diff.items()
+        ],
+        axis=0)
+    # get final indices of changed elements of b
+    changed_b_indices = np.concatenate(
+        [
+            np.asarray(v) + b_file_offsets[k] for k,
+            v in b_indices_in_diff.items()
+        ],
+        axis=0)
+    # get final indices of unchanged elements of a
+    unchanged_a_indices = np.concatenate(
+        [
+            np.asarray(v) + a_file_offsets[k] for k,
+            v in a_indices_not_in_diff.items()
+        ],
+        axis=0)
+    # get final indices of unchanged elements of b
+    unchanged_b_indices = np.concatenate(
+        [
+            np.asarray(v) + b_file_offsets[k] for k,
+            v in b_indices_not_in_diff.items()
+        ],
+        axis=0)
+    # Compute alignment
+    # Unchanged indices should map one-to-one in ascending order.
+    # Any dropped or added content will have been handled in
+    # diff_alignment.
+    no_diff_alignment: np.ndarray = np.stack(
+        [unchanged_a_indices,
+         unchanged_b_indices],
+        axis=-1)
+    # remap alignment of changed items to use final indices
+    diff_alignment = np.stack(
+        [
+            changed_a_indices[diff_alignment[:,
+                                             0]],
+            changed_b_indices[diff_alignment[:,
+                                             1]]
+        ],
+        axis=-1)
+    alignment = np.concatenate([no_diff_alignment, diff_alignment], axis=0)
     return alignment
