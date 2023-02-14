5bc0078631e2a7e38ed55349093770fe7e2a8335
Andrew Gardner <Andrew.Gardner@radiancetech.com>
2023-02-14 20:04:05 +0000

    Check goals for equality in test_repair_instance after patching

--- prism/data/repair/tests/test_instance.py
+++ prism/data/repair/tests/test_instance.py
@@ -10,6 +10,7 @@ from prism.data.build_cache import ProjectCommitData
 from prism.data.repair.align import default_align
 from prism.data.repair.diff import compute_git_diff
 from prism.data.repair.instance import ProjectCommitDataDiff
+from prism.interface.coq.goals import Goal, GoalLocation
 from prism.tests import _DATA_PATH
 
 
@@ -26,10 +27,32 @@ class TestRepairInstance(unittest.TestCase):
             a: ProjectCommitData,
             b: ProjectCommitData) -> None:
         """
-        Assert two commits have equivalent text.
+        Assert two commits have equivalent text and goals.
         """
         git_diff = compute_git_diff(a, b)
         self.assertEqual(git_diff.text, "")
+        # verify goals are equal
+        b_sentences = b.sorted_sentences()
+        for afile, asentences in a.sorted_sentences().items():
+            bsentences = b_sentences[afile]
+            for asentence, bsentence in zip(asentences, bsentences):
+                if asentence.goals is None:
+                    self.assertIsNone(bsentence.goals)
+                else:
+                    assert bsentence.goals is not None
+                    for ag, bg in zip(asentence.goals, bsentence.goals):
+                        if isinstance(ag, (Goal, GoalLocation)):
+                            # a goal or removed goal
+                            self.assertEqual(str(ag), str(bg))
+                        else:
+                            self.assertIsInstance(bg, tuple)
+                            if isinstance(ag[0], Goal):
+                                # an added goal
+                                self.assertEqual(str(ag[0]), str(bg[0]))
+                                self.assertEqual(ag[1], bg[1])
+                            else:
+                                # a moved goal
+                                self.assertEqual(ag, bg)
 
     def test_ProjectCommitDataDiff(self) -> None:
         """
