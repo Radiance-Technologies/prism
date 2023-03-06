04667b2cbaafe3de60c06f90343f0dd533a20997
Andrew Gardner <Andrew.Gardner@radiancetech.com>
2023-03-06 22:28:26 +0000

    Add optional location information to CoqExn

--- prism/interface/coq/exception.py
+++ prism/interface/coq/exception.py
@@ -2,7 +2,9 @@
 Exceptions related to interacting with Coq through SerAPI.
 """
 
-from typing import Tuple, Union
+from typing import Optional, Tuple, Type
+
+from prism.language.gallina.analyze import SexpInfo
 
 
 class CoqExn(Exception):
@@ -10,7 +12,11 @@ class CoqExn(Exception):
     An error raised within Coq and lifted to a Python exception.
     """
 
-    def __init__(self, msg: str, full_sexp: str):
+    def __init__(
+            self,
+            msg: str,
+            full_sexp: str,
+            location: Optional[SexpInfo.Loc] = None):
         super().__init__()
         self.msg = msg
         """
@@ -20,9 +26,17 @@ class CoqExn(Exception):
         """
         The full S-expression containing the error yielded from SerAPI.
         """
+        self.location = location
+        """
+        The source code location of the error.
+        """
 
-    def __reduce__(self) -> Union[str, Tuple[str, str]]:  # noqa: D105
-        return CoqExn, (self.msg, self.full_sexp)
+    def __reduce__(  # noqa: D105
+        self) -> Tuple[Type['CoqExn'],
+                       Tuple[str,
+                             str,
+                             Optional[SexpInfo.Loc]]]:
+        return CoqExn, (self.msg, self.full_sexp, self.location)
 
     def __repr__(self) -> str:  # noqa: D105
         return str(self)
@@ -31,7 +45,17 @@ class CoqExn(Exception):
         """
         Get the Coq error message.
         """
-        return self.msg
+        if self.location is None:
+            msg = self.msg
+        else:
+            msg = ''.join(
+                [
+                    self.msg,
+                    "\n",
+                    f"In file {self.location.filename},"
+                    f"lines {self.location.lineno}-{self.location.lineno_last}"
+                ])
+        return msg
 
 
 class CoqTimeout(Exception):
