f3bdde3c5816f1a4e8f4eccfccea65b8db689cfc
Wesley Henderson <robert.henderson@radiancetech.com>
2023-03-06 11:12:43 -0600

    Updated mining script and default config json

--- scripts/data/mine_repair_instances.py
+++ scripts/data/mine_repair_instances.py
@@ -6,11 +6,58 @@ import json
 from pathlib import Path
 from typing import Any, Dict, Optional
 
-from prism.data.repair.mining import repair_mining_loop
+from prism.data.repair.instance import (
+    ProjectCommitDataErrorInstance,
+    ProjectCommitDataRepairInstance,
+)
+from prism.data.repair.mining import prepare_label_pairs, repair_mining_loop
 
 
-def _resolve_python_objects(config: Dict[str, Any]) -> Dict[str, Any]:
-    ...
+def _process_config(config: Dict[str, Any]) -> Dict[str, Any]:
+    """
+    Process script config file and resolve python objects.
+
+    For better security, don't allow resolving arbitrary callable
+    imports. Restrict to those given in internal mappings.
+
+    Parameters
+    ----------
+    config : Dict[str, Any]
+        Config file dictionary
+
+    Returns
+    -------
+    Dict[str, Any]
+        Updated config file dictionary
+    """
+    prepare_pairs_map = {
+        "prism.data.repair.mining.prepare_label_pairs": prepare_label_pairs
+    }
+    repair_miner_map = {
+        "prism.data.repair.instance.ProjectCommitDataRepairInstance":
+            ProjectCommitDataRepairInstance.make_repair_instance
+    }
+    changeset_miner_map = {
+        "prism.data.repair.instance.ProjectCommitDataErrorInstance.default_changeset_miner":
+            ProjectCommitDataErrorInstance.default_changeset_miner
+    }
+    if "cache_root" in config:
+        config["cache_root"] = Path(config["cache_root"])
+    if "repair_save_directory" in config:
+        config["repair_save_directory"] = Path(config["repair_save_directory"])
+    if "prepare_pairs" in config:
+        config["prepare_pairs"] = prepare_pairs_map.get(
+            config["prepare_pairs"],
+            None)
+    if "repair_miner" in config:
+        config["repair_miner"] = repair_miner_map.get(
+            config["repair_miner"],
+            None)
+    if "changeset_miner" in config:
+        config["changeset_miner"] = changeset_miner_map.get(
+            config["changeset_miner"],
+            None)
+    return config
 
 
 def load_config_json(config_file_path: Optional[str]) -> Dict[str, Any]:
@@ -29,12 +76,12 @@ def load_config_json(config_file_path: Optional[str]) -> Dict[str, Any]:
         resolved to their actual Python objects.
     """
     if config_file_path is None:
-        config_file_path = Path(__file__).parent / "mine_repair_instances.json"
+        config_file_path = Path(
+            __file__).parent / "mine_repair_instances_default_config.json"
     else:
         config_file_path = Path(config_file_path)
     with open(config_file_path, "r") as f:
-        config = json.load(f)
-    config = _resolve_python_objects(config)
+        config = _process_config(json.load(f))
     return config
 
 
@@ -43,7 +90,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--config-json",
         default=None,
-        help="Location of yaml configuration file for this script.")
+        help="Location of json configuration file for this script.")
     args = parser.parse_args()
     kwargs = load_config_json(args.config_json)
     repair_mining_loop(**kwargs)
