42f115f04469e23ccdb23021415157f5cd982763
Wesley Henderson <robert.henderson@radiancetech.com>
2023-03-20 15:08:45 -0500

    Un-pin opam switch in test_extract_cache

--- prism/data/tests/test_extract_cache.py
+++ prism/data/tests/test_extract_cache.py
@@ -57,7 +57,7 @@ class TestExtractCache(unittest.TestCase):
     TEST_DIR = Path(__file__).parent
     CACHE_DIR = TEST_DIR / "project_build_cache"
     dir_list: List[Path]
-    test_switch: OpamSwitch = OpamSwitch("prism-8.10.2")
+    test_switch: OpamSwitch = OpamSwitch()
     serapi_version: str
     update_8_14: bool
     """
