fd651f602cdee46020dcb5b07ee15848aa97942e
Andrew Touchet <andrew.touchet@radiancetech.com>
2023-03-23 15:07:43 -0500

    use more robust typevar extraction

--- prism/util/manager.py
+++ prism/util/manager.py
@@ -3,40 +3,9 @@ Module for Manager based Client/Server.
 """
 import typing
 from multiprocessing.managers import BaseManager
-from typing import Generic, GenericAlias, Type, TypeVar
+from typing import Generic, Type, TypeVar
 
-from typing_extensions import Protocol, TypeGuard
-
-
-class IndirectGenericSubclass(Protocol):
-    """
-    Type hint for generic subclass.
-    """
-
-    __orig_bases__: tuple[GenericAlias]
-
-
-def is_indirect_generic_subclass(
-    obj: object,
-) -> TypeGuard[IndirectGenericSubclass]:
-    """
-    Determine if obj is subclass of Generic.
-    """
-    if hasattr(obj, '__orig_bases__'):
-        bases = obj.__orig_bases__
-        return bases is not None and isinstance(bases, tuple)
-    return False
-
-
-def get_generic_args(cls):
-    """
-    Get types passed to generic arguments.
-    """
-    if not is_indirect_generic_subclass(cls):
-        raise ValueError(
-            'Expected class type to have an `__orig_bases__` attribute')
-    return typing.get_args(cls.__orig_bases__[0])[0]
-
+from prism.util.serialize import get_typevar_bindings
 
 ManagedClient = TypeVar('ManagedClient')
 
@@ -56,7 +25,8 @@ class ManagedServer(BaseManager, Generic[ManagedClient]):
         """
         Register logger with base manager.
         """
-        if cls is not ManagedServer:
-            cls.Client = get_generic_args(cls)
-            cls.register(cls.Client.__name__, cls.Client)
+        cls.Client = typing.cast(
+            Type[ManagedClient],
+            get_typevar_bindings(cls)[1][ManagedClient])
+        cls.register(cls.Client.__name__, cls.Client)
         return super().__new__(cls, *args, **kwargs)
