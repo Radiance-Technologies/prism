4c9689bfb20b734dc024acb1b61e8c0e2e7b3b97
Wesley Henderson <robert.henderson@radiancetech.com>
2023-04-01 16:00:23 -0500

    Four things: * Move ExecutionError to exception module * Remove redundant cwd arg * Fix outdated docstrings * Raise error on missing serapi_options instead of trying to reinfer them in build_line_by_line

--- prism/project/exception.py
+++ prism/project/exception.py
@@ -4,6 +4,8 @@ Defines exceptions related to project management.
 
 from typing import Tuple, Union
 
+from prism.language.gallina.analyze import SexpInfo
+
 
 class DirHasNoCoqFiles(Exception):
     """
@@ -14,6 +16,25 @@ class DirHasNoCoqFiles(Exception):
     """
 
     pass
+
+
+class ExecutionError(Exception):
+    """
+    Alert that an unexpected error has occurred during execution.
+
+    For example, this may be raised if an error is encountered when
+    trying to reach a designated region of repair.
+    """
+
+    def __init__(self, fail_loc: SexpInfo.Loc, error_msg: str) -> None:
+        self.fail_loc = fail_loc
+        self.error_msg = error_msg
+
+    def __str__(self) -> str:  # noqa: D105
+        return (
+            f"In {self.fail_loc.filename}, "
+            f"lines {self.fail_loc.lineno}-{self.fail_loc.lineno_last}: "
+            f"{self.error_msg}")
 
 
 class MissingMetadataError(Exception):
