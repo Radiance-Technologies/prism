ea1566e1b7c54661f5c0a46af6b08cd23c49f67a
Andrew Gardner <Andrew.Gardner@radiancetech.com>
2023-06-20 15:27:16 +0000

    Fix last line boundary check for whether a location includes an empty change

--- prism/data/repair/diff.py
+++ prism/data/repair/diff.py
@@ -57,7 +57,8 @@ def is_location_in_change(
             or loc.contains_lineno(change_range.stop - 1)))
     loc_contains_empty_change = (
         not is_change_nonempty and loc.contains_lineno(change_range.start)
-        and change_range.start != loc.lineno)
+        and change_range.start != loc.lineno
+        and change_range.start != loc.lineno_last)
     return change_filename == loc.filename and (
         # if there is any intersection, then one endpoint lies in the
         # interval spanned by the change
